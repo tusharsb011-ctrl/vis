dc67a9bf1ed9e5ff43318ebb8f1f2a4977c6f38a
tusharsb011 <tusharsb011@gmail.com>
2026-05-23 16:58:34 +0530

    Fix Vercel serverless function module format

--- api/topics.js
+++ api/topics.js
@@ -1,6 +1,6 @@
 const { kv } = require('@vercel/kv');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method === 'GET') {
     try {
       let topics = [];
@@ -26,4 +26,4 @@ export default async function handler(req, res) {
   } else {
     return res.status(405).json({ error: 'Method not allowed' });
   }
-}
+};
